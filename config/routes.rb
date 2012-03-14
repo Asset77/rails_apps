9a0122d7b46c974446188a68fab5d7173e79115c
Minh <bro19742010@gmail.com>
2012-03-13 15:11:09 -1000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+# config/routes.rb
+Proj2a::Application.routes.draw do
+  get "students/index"
+  match "students/newStudent" => "students#newStudent"
+  match "students/createStudent" => "students#createStudent"
+  match "students/selectStudent" => "students#selectStudent"
+  match "students/editStudent" => "students#editStudent"
+end
